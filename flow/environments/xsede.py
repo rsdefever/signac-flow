3376c2c512429f81b3cff33e3ada90f6506f3bcd
Pengji Zhou <zhoupj@umich.edu>
2018-11-02 19:41:48 -0400

    move new environment under xsede

--- flow/environments/xsede.py
+++ flow/environments/xsede.py
@@ -151,5 +151,47 @@ class BridgesEnvironment(DefaultSlurmEnvironment):
           default='RM-shared',
           help="Specify the partition to submit to.")
 
+class Stampede2LauncherEnvironment(Stampede2Environment):
+    """Environment profile for the Stampede2 supercomputer using launcher.
+    https://www.tacc.utexas.edu/systems/stampede2
+    """
+    template = 'stampede2launcher.sh'
 
-__all__ = ['CometEnvironment', 'BridgesEnvironment', 'Stampede2Environment']
+    @classmethod
+    def submit(cls, _id, script, flags=None, *args, **kwargs):
+        """Submit a job submission script to the environment's scheduler.
+
+        Scripts should be submitted to the environment, instead of directly
+        to the scheduler to allow for environment specific post-processing.
+        """
+        if flags is None:
+            flags = []
+        env_flags = getattr(cls, 'submit_flags', [])
+        if env_flags:
+            flags.extend(env_flags)
+
+        # Hand off the actual submission to the scheduler
+        if isinstance(script, JobScript):  # api version < 6
+            script = str(script)
+
+        submission_name=_id.split('/')[-1]
+        dir='launcher/'
+        launcher_part='''
+export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
+export LAUNCHER_RMI=SLURM
+export LAUNCHER_JOB_FILE=%s
+
+$LAUNCHER_DIR/paramrun
+''' %(dir+submission_name)
+        script=script+launcher_part
+
+        # create list of commands file for launcher
+        launcher_file  = open('%s' %(dir+submission_name), 'w')
+        for operation in operations:
+            file_object.write(operation.cmd)
+        launcher_file.close()
+
+        if cls.get_scheduler().submit(script, flags=flags, *args, **kwargs):
+            return JobStatus.submitted
+
+__all__ = ['CometEnvironment', 'BridgesEnvironment', 'Stampede2Environment', 'Stampede2LauncherEnvironment']
